8f530d148f5a5c31661085f0e5109ff5aad8ca32
kismatdahal24@gmail.com <kismatdahal24@gmail.com>
2026-07-11 15:12:18 +0545

    numpy

---
+++ numpy/broadcastin&/2ddd.py
@@ -0,0 +1,5 @@
+import numpy as np
+num = np.array([[1, 2], [3, 4], [5, 6]])
+num2 = np.array([1,2 ])
+total = num + num2
+print(total)
---
+++ numpy/vectorization.py/vector.py
@@ -0,0 +1,6 @@
+import numpy as np
+num = np.array([[1, 2], [3, 4], [5, 6]])
+num2 = num +2 
+num3= num*2
+print(num3)
+print(num2)
