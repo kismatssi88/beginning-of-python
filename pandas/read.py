ff43b325d79326924ca2acfbb7e12d2946faaeff
kismatdahal24@gmail.com <kismatdahal24@gmail.com>
2026-07-14 19:44:36 +0545

    message

---
+++ pandas/read.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv("pandas/annual.csv")
+print(df.head())
--- pandas/saving/paan.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("pandas/employees.csv")
-print(df)
-print(df.head(10))
-print(df.tail(10))
