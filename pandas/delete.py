7794f0c9272f8b28ba4d01a72cb1117fcf197f04
kismatdahal24@gmail.com <kismatdahal24@gmail.com>
2026-07-17 17:50:32 +0545

    deletin

---
+++ pandas/delete.py
@@ -0,0 +1,13 @@
+import pandas as pd
+footballers={
+    "Name": ["Lionel Messi","yamal","Cristiano Ronaldo","Neymar Jr","Kylian Mbappe"],
+    "Age": [39, 19, 41, 32, 28],
+    "Nationality": ["Argentinian", "Spanish", "Portuguese", "Brazilian", "French"],
+    "position": ["Right Winger", "right Winger", "striker", "Left Winger", "striker"],
+    "preferred_foot": ["Left", "left", "Right", "Left", "Right"],
+    "fifa rating": [91, 87, 90, 88, 92],
+}
+df = pd.DataFrame(footballers)
+print(df)
+df.drop(columns=["position"],inplace=True)
+print(df)
